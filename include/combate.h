2107b34ddb41c48c1e2724d0f93bb9b873ba97b7
valentinotrabajo1-cell <valentinotrabajo1@gmail.com>
2026-05-20 18:54:44 -0500

    Update combate.h

--- include/combate.h
+++ include/combate.h
@@ -1,1 +1,14 @@
+#ifndef COMBATE_H
+#define COMBATE_H
+
+#include "entidades.h"
+
+
+int jugador_ataca(
+    const Jugador* jugador,
+    Enemigo enemigos[],
+    int num_enemigos,
+    Estadisticas* stats
+);
+
 
