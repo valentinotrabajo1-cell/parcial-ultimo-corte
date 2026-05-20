ddbde4ca8b0ab9194b226e26dd1bf9a968138ab0
valentinotrabajo1-cell <valentinotrabajo1@gmail.com>
2026-05-20 18:37:11 -0500

    Update constantes.h

--- include/constantes.h
+++ include/constantes.h
@@ -21,3 +21,28 @@
 
 
 
+#define TECLA_ARRIBA   'w'
+#define TECLA_ABAJO    's'
+#define TECLA_IZQ      'a'
+#define TECLA_DER      'd'
+#define TECLA_RECOGER  'e'
+#define TECLA_SOLTAR   'q'
+#define TECLA_SALIR    'x'
+#define TECLA_ATACAR   'f'   // Nueva: atacar enemigos cercanos
+#define TECLA_MAPA     'm'   // Nueva: mostrar minimapa
+
+
+#define VIDA_JUGADOR       5
+#define DANO_GOBLIN        1
+#define DANO_SLIME         1
+#define CURACION_ITEM      2
+
+
+#define VIDA_GOBLIN        2   // El goblin necesita 2 golpes para morir
+#define VIDA_SLIME         3   // El slime necesita 3 golpes para morir
+#define DANO_JUGADOR       1   // El jugador hace 1 punto de dano por golpe
+
+
+#define RANGO_ATAQUE       2
+
+#endif
