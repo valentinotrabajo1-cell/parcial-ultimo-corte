aa650a5124bfbbcad8dc9feef8020d4c8458cc19
valentinotrabajo1-cell <valentinotrabajo1@gmail.com>
2026-05-20 18:36:44 -0500

    Update constantes.h

--- include/constantes.h
+++ include/constantes.h
@@ -9,7 +9,14 @@
 #define MAX_ITEMS    6
 #define MAX_SALAS    8
 
-
+#define SIM_PARED      '#'
+#define SIM_SUELO      '.'
+#define SIM_JUGADOR    '@'
+#define SIM_ITEM       '!'
+#define SIM_ENEMIGO_A  'G'   
+#define SIM_ENEMIGO_B  'S'   
+#define SIM_PUERTA     'D'
+#define SIM_META       'E'  
 
 
 
