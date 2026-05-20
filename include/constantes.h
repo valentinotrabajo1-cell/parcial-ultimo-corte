f269222414401533ff774f6a4eb3d9e50bf3f104
valentinotrabajo1-cell <valentinotrabajo1@gmail.com>
2026-05-20 16:48:06 -0500

    Create constantes.h

---
+++ include/constantes.h
@@ -0,0 +1,16 @@
+#ifndef CONSTANTES_H
+#define CONSTANTES_H
+
+
+#define ANCHO_SALA   20
+#define ALTO_SALA    10
+
+#define MAX_ENEMIGOS 10
+#define MAX_ITEMS    6
+#define MAX_SALAS    8
+
+
+
+
+
+
