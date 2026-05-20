4199241ed09bbc0dde2cc35cb73c37025976306f
valentinotrabajo1-cell <valentinotrabajo1@gmail.com>
2026-05-20 18:42:00 -0500

    Update entidades.h

--- include/entidades.h
+++ include/entidades.h
@@ -1,1 +1,19 @@
-aaa
+#ifndef ENTIDADES_H
+#define ENTIDADES_H
+
+#include "constantes.h"
+
+
+struct Jugador {
+    int x;           
+    int y;          
+    int vida;
+    int sala_actual; 
+    bool tiene_item; 
+};
+
+
+enum TipoEnemigo {
+    GOBLIN,   
+    SLIME     
+};
