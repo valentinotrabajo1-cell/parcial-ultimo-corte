2ae7b73574a322565a7dc639b4bfa55cad886640
valentinotrabajo1-cell <valentinotrabajo1@gmail.com>
2026-05-20 18:49:02 -0500

    Update juego.h

--- include/juego.h
+++ include/juego.h
@@ -10,4 +10,18 @@ int verificar_victoria(const Jugador* jugador, const Sala salas[]);
 int verificar_derrota(const Jugador* jugador);
 
 
+void mostrar_victoria();
 
+
+void mostrar_derrota();
+
+
+void limpiar_pantalla();
+
+
+void pausar();
+
+
+char leer_tecla();
+
+#endif
