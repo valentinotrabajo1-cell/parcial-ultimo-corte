6c9f787764b2935c61195d154a00e0d4da447939
valentinotrabajo1-cell <valentinotrabajo1@gmail.com>
2026-05-20 18:57:48 -0500

    Update enemigo.h

--- include/enemigo.h
+++ include/enemigo.h
@@ -2,3 +2,24 @@
 #define ENEMIGO_H
 
 
+#include "entidades.h"
+
+void inicializar_enemigos(Enemigo enemigos[], int* num_enemigos);
+
+
+void mover_enemigos(
+    Enemigo enemigos[],
+    int num_enemigos,
+    const Jugador* jugador,
+    const Sala salas[]
+);
+
+
+void atacar_jugador(
+    const Enemigo enemigos[],
+    int num_enemigos,
+    Jugador* jugador
+);
+
+#endif
+
