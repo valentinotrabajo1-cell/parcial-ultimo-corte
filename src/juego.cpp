533ce7b6903a752f7b0cb8ee0835ea2fc353d1b3
nicolvanegas506-prog <nicolvanegas506@gmail.com>
2026-05-20 21:26:27 -0500

    Add platform-specific screen clearing function

--- src/juego.cpp
+++ src/juego.cpp
@@ -2,3 +2,19 @@
 #include "../include/constantes.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+// Incluimos los encabezados necesarios segun el sistema operativo
+#ifdef _WIN32
+    #include <conio.h>    // para _getch() en Windows
+#else
+    #include <termios.h>  // para configurar la terminal en Linux/Mac
+    #include <unistd.h>
+#endif
+
+void limpiar_pantalla() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
