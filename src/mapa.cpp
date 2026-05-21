e55e067f7cf37f4cfb34590ff163995a028d7701
nicolvanegas506-prog <nicolvanegas506@gmail.com>
2026-05-20 21:33:31 -0500

    Document dungeon structure in mapa.cpp
    Added comments explaining the dungeon layout and room connections.

--- src/mapa.cpp
+++ src/mapa.cpp
@@ -1,1 +1,27 @@
+#include "../include/mapa.h"
+#include "../include/constantes.h"
+#include <stdio.h>
+#include <string.h>
+
+/*
+  Cada sala se construye como una cuadricula de caracteres.
+  '#' = pared, '.' = suelo, 'D' = puerta, 'E' = meta.
+
+  El dungeon tiene 6 salas conectadas asi:
+      [0] -- [1] -- [2]
+               |
+              [3] -- [4]
+               |
+              [5]
+
+  Las salas 0..5 son las minimas requeridas.
+  Ademas se agregan 2 extra (6 y 7) para dar mas profundidad.
+
+      [0] -- [1] -- [2] -- [6]
+               |
+              [3] -- [4]
+               |
+              [5] -- [7]  <-- sala con la meta (escalera)
+*/
+
 
