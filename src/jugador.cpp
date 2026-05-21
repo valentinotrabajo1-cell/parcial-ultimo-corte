d01d97d9dc6eb07c6eae4b34f7a6ed4032fd7c83
nicolvanegas506-prog <nicolvanegas506@gmail.com>
2026-05-20 21:29:51 -0500

    Update jugador.cpp

--- src/jugador.cpp
+++ src/jugador.cpp
@@ -2,3 +2,13 @@
 #include "../include/mapa.h"
 #include "../include/constantes.h"
 #include <stdio.h>
+
+void inicializar_jugador(Jugador* jugador) {
+    // El jugador empieza en la sala 0, posicion (2, 5)
+    jugador->x = 2;
+    jugador->y = 5;
+    jugador->vida = VIDA_JUGADOR;
+    jugador->sala_actual = 0;
+    jugador->tiene_item = 0;
+}
+
