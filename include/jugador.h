17ecdfe2045925091d295da5e669db60256289ea
valentinotrabajo1-cell <valentinotrabajo1@gmail.com>
2026-05-20 19:13:27 -0500

    Update jugador.h

--- include/jugador.h
+++ include/jugador.h
@@ -1,1 +1,27 @@
+#ifndef JUGADOR_H
+#define JUGADOR_H
+
+#include "entidades.h"
+
+
+void inicializar_jugador(Jugador* jugador);
+
+
+int mover_jugador(
+    Jugador* jugador,
+    char direccion,
+    const Sala salas[],
+    int num_salas
+);
+
+
+void revisar_item(Jugador* jugador, Item items[], int num_items);
+
+
+void soltar_item(Jugador* jugador, Item items[], int num_items);
+
+
+void mostrar_estado(const Jugador* jugador);
+
+#endif
 
