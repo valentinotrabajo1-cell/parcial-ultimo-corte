d5bd970e776f18a35062c80e9d15cf861ddbc653
nicolvanegas506-prog <nicolvanegas506@gmail.com>
2026-05-20 21:37:49 -0500

    Initialize minimapa.cpp with header and comments
    Added header comments and includes for minimapa implementation.

--- src/minimapa.cpp
+++ src/minimapa.cpp
@@ -1,1 +1,24 @@
+#include "../include/minimapa.h"
+#include "../include/constantes.h"
+#include <stdio.h>
+
+/*
+  minimapa.cpp
+  ------------
+  Implementacion del minimapa esquematico del dungeon.
+
+  Usamos un arreglo estatico de enteros para saber que salas
+  el jugador ya visito. 1 = visitada, 0 = no visitada.
+
+  El mapa del dungeon se ve asi:
+
+      Sala 0 -- Sala 1 -- Sala 2 -- Sala 6
+                  |
+                Sala 3 -- Sala 4
+                  |
+                Sala 5 -- Sala 7 (META)
+
+  Lo dibujamos con caracteres ASCII sencillos.
+*/
+
 
