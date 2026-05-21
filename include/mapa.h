3ccfead1bab70c8eccd26801150f5757dae7dbff
valentinotrabajo1-cell <valentinotrabajo1@gmail.com>
2026-05-20 19:13:51 -0500

    Update mapa.h

--- include/mapa.h
+++ include/mapa.h
@@ -1,1 +1,23 @@
+#ifndef MAPA_H
+#define MAPA_H
 
+#include "entidades.h"
+
+
+void inicializar_salas(Sala salas[], int num_salas);
+
+
+void dibujar_sala(
+    const Sala salas[],
+    int sala_actual,
+    const Jugador* jugador,
+    const Enemigo enemigos[],
+    int num_enemigos,
+    const Item items[],
+    int num_items
+);
+
+
+int es_pared(const Sala salas[], int sala, int x, int y);
+
+#endif
